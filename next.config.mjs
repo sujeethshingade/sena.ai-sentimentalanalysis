822c8d35aa5ec1aebfef4dbc3c472734c3ee26a6
Sujeeth Shingade <108385080+sujeethshingade@users.noreply.github.com>
2024-07-13 17:57:51 +0530

    Start

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination:
+            process.env.NODE_ENV === 'development'
+              ? 'http://127.0.0.1:5328/api/:path*'
+              : '/api/:path*',
+        },
+      ]
+    },
+  }
+  
+  export default nextConfig
+  
