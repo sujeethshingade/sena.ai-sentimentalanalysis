569997e5fef50c911f97a3eaf046add3fdb10ccf
Sujeeth Shingade <108385080+sujeethshingade@users.noreply.github.com>
2024-07-13 21:40:38 +0530

    proper change

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
 'use client';
 
-import { SetStateAction, useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import { motion } from 'framer-motion';
 
 export default function Home() {
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState<File | null>(null);
 
-  const handleFileChange = (e: { target: { files: SetStateAction<null>[]; }; }) => {
-    setFile(e.target.files[0]);
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setFile(e.target.files[0]);
+    }
   };
 
   const handleUpload = async () => {
     try {
+      if (!file) {
+        console.error('No file selected.');
+        return;
+      }
+
       const formData = new FormData();
       formData.append('file', file);
 
